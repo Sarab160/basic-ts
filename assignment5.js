bd70d38eddbaf5a5125367fb19e3da2d47028d0b
Sarab160 <142167410+Sarab160@users.noreply.github.com>
2023-09-09 23:53:37 +0500

    data uploaded

---
+++ assignment5.js
@@ -0,0 +1,106 @@
+"use strict";
+// 2
+var stud = "Eric";
+// console.log('hello',stud,"would you like learn phython today");
+//3
+var personsName = ["sarab", "wasif", "cheema"];
+/*console.log("lowercase",personsName[0]);
+console.log('uppercase',personsName[1]);
+console.log("titlecase",personsName[2]);*/
+//4
+var aurthur = "Albert Einstein";
+var quote = "A person who never made a mistake never tried anything new.";
+// console.log(aurthur,"once said,",`"${quote}"`);
+//5
+let famousPerson = "Sarab";
+let message = "everyone will get what he works for";
+// console.log(message);
+//7
+var addition = 6 + 2;
+//console.log("Addition result =",addition);
+var subtraction = 12 - 4;
+//console.log("subtraction result =",subtraction);
+var multiplication = 4 * 2;
+//console.log("multiplication result =",multiplication);
+var divison = 64 / 8;
+//console.log("divison result =",divison);
+//25
+var alliencolour = "green";
+if (alliencolour == "green") {
+    //console.log("player just earned 5 points");
+}
+else {
+}
+//26
+var alliencolour1 = "green";
+if (alliencolour1 == "green") {
+    //console.log("player just earned 5 points");
+}
+else {
+    //console.log("playes just earned 10 points");
+}
+var alliencolour2 = "red";
+if (alliencolour2 == "green") {
+    //console.log("player just earned 5 points");
+}
+else {
+    //console.log("player just earned 10 points");
+}
+//27
+var alliencolour3 = "green";
+if (alliencolour3 == "green") {
+    // console.log("player just earned 5 points");
+}
+var alliencolour4 = "red";
+if (alliencolour4 == "red") {
+    // console.log("player earned 15 points");
+}
+var alliencolour5 = "yellow";
+if (alliencolour5 == "yellow") {
+    // console.log("player just earned 10 points");
+}
+//28
+var age = 20;
+if (age < 2) {
+    console.log("baby");
+}
+else if (age == 2 && age < 4) {
+    console.log("toddler");
+}
+else if (age == 4 && age < 13) {
+    console.log("kid");
+}
+else if (age == 13 && age < 20) {
+    console.log("teenager");
+}
+else if (age == 20 && age < 65) {
+    //console.log("adult");
+}
+else {
+    console.log("older");
+}
+//29
+var fruits = ["Apple", "Mango", "Banana"];
+if (fruits[0] == "Apple") {
+    // console.log("you really like Apple!");
+}
+if (fruits[1] == "grapes") {
+    // console.log("yu like grapes!");
+}
+if (fruits[1] == "Mango") {
+    // console.log("you like Mango!");
+}
+if (fruits[2] == "Banana") {
+    // console.log("you like banana!");
+}
+if (fruits[0] == "pineapple") {
+    console.log("yiu like pineapple!");
+}
+//30
+var userNames = ["Admin", "Sarab", "Saqib", "wasif", "taqi"];
+if (userNames[0] == "Admin") {
+    console.log("Hello Admin would you like to see a status report?");
+}
+else {
+    console.log("hello");
+}
---
+++ assignment5.ts
@@ -0,0 +1,117 @@
+// 2
+var stud:string = "Eric"
+// console.log('hello',stud,"would you like learn phython today");
+//3
+var personsName:string[] = ["sarab","wasif","cheema"];
+/*console.log("lowercase",personsName[0]);
+console.log('uppercase',personsName[1]);
+console.log("titlecase",personsName[2]);*/
+//4
+var aurthur:string = "Albert Einstein";
+var quote:string = "A person who never made a mistake never tried anything new."
+// console.log(aurthur,"once said,",`"${quote}"`);
+//5
+let famousPerson:string = "Sarab";
+let message:string = "everyone will get what he works for";
+// console.log(message);
+//7
+var addition:number = 6+2;
+//console.log("Addition result =",addition);
+var subtraction:number = 12-4;
+//console.log("subtraction result =",subtraction);
+var multiplication:number = 4*2;
+//console.log("multiplication result =",multiplication);
+var divison:number = 64/8;
+//console.log("divison result =",divison);
+//25
+var alliencolour:string= "green"
+if(alliencolour=="green"){
+    //console.log("player just earned 5 points");
+}
+else{
+
+}
+//26
+var alliencolour1:string = "green";
+if(alliencolour1=="green"){
+    //console.log("player just earned 5 points");
+}
+else{
+    //console.log("playes just earned 10 points");
+    
+}
+var alliencolour2:string = "red";
+if(alliencolour2=="green"){
+    //console.log("player just earned 5 points");
+}
+else{
+    //console.log("player just earned 10 points");
+    
+}
+//27
+var alliencolour3:string = "green";
+if(alliencolour3=="green"){
+    // console.log("player just earned 5 points");
+}
+var alliencolour4:string = "red";
+if(alliencolour4=="red"){
+    // console.log("player earned 15 points");
+}
+var alliencolour5:string= "yellow";
+if(alliencolour5=="yellow"){
+    // console.log("player just earned 10 points");
+} 
+//28
+var age:number  = 20;
+if(age<2){
+    console.log("baby");
+}
+else if(age==2&&age<4){
+    console.log("toddler");
+}
+else if(age==4&&age<13){
+    console.log("kid");
+}
+else if(age==13&&age<20){
+    console.log("teenager");
+}
+else if(age==20&&age<65){
+    //console.log("adult");
+}
+else{
+    console.log("older");
+}
+//29
+var fruits:string[] = ["Apple","Mango","Banana"];
+if(fruits[0]=="Apple"){
+   // console.log("you really like Apple!");
+}
+ if(fruits[1]=="grapes"){
+   // console.log("yu like grapes!");
+}
+ if(fruits[1]=="Mango"){
+   // console.log("you like Mango!");
+}
+ if(fruits[2]=="Banana"){
+   // console.log("you like banana!");
+}
+ if(fruits[0]=="pineapple"){
+    console.log("yiu like pineapple!");
+}
+//30
+var userNames:string[] = ["Admin","Sarab","Saqib","wasif","taqi"];
+if(userNames[0]=="Admin"){
+    console.log("Hello Admin would you like to see a status report?") 
+}
+else{
+    console.log("hello"); 
+}
+//21
+let car ={
+    company:"toyata",
+    model:"v8",
+    colour:"black",
+    modelyear:2023,
+    inuse:false
+}
+console.log(car);
